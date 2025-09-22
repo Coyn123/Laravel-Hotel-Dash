dec73ebbf9d4d07ca42d7a4d41abf961ef9f6242
Coyn123 <61031960+Coyn123@users.noreply.github.com>
2025-09-21 21:44:27 -0400

    Livewire Update
    non-functioning Livewire implementation, need to transition view files to Livewire functionality

--- hoteldash/routes/web.php
+++ hoteldash/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
 // Dashboard page
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 // Handle setup form submission on first run
 Route::post('/setup', [DashboardController::class, 'storeSetup'])->name('setup.store');
