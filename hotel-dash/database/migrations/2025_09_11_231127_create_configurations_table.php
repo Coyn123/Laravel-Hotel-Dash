928f6e25980f7f31b0f87792a527b0c10fbf12ab
Coyn123 <61031960+Coyn123@users.noreply.github.com>
2025-09-23 10:56:56 -0400

    Database Schema Update
    More normalized and consistent database schema to further populate and configure as we scale

--- hotel-dash/database/migrations/2025_09_11_231127_create_configurations_table.php
+++ hotel-dash/database/migrations/2025_09_11_231127_create_configurations_table.php
@@ -11,13 +11,36 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('configurations', function (Blueprint $table) {
+        Schema::create('properties_config', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('property_name', 100);
-            $table->string('floor_name', 100);
-            $table->integer('floor_number');
-            $table->integer('floor_count');
-            $table->integer('aux_property_count');
+            $table->string('property_address', 100);
+            $table->integer('num_of_floors');
+            $table->integer('num_of_rooms');
+            $table->timestamps();
+        });
+        Schema::create('floors_config', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('property_id', 100);
+            $table->string('floor_num', 100);
+            $table->integer('floor_range_bot');
+            $table->integer('floor_range_top');
+            $table->timestamps();
+        });
+        Schema::create('rooms_config', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('property_id', 100);
+            $table->string('room_type', 100);
+            $table->integer('room');
+            $table->string('room_status', 100);
+            $table->timestamps();
+        });
+        Schema::create('aux_property_conifg', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('property_id');
+            $table->string('aux_name', 100);
+            $table->integer('aux_type');
+            $table->string('aux_status', 100);
             $table->timestamps();
         });
     }
